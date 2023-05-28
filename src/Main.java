90e423a99a66940e56e95944a9d39523667f041e
Grisha <guruwolf8@gmail.com>
2023-05-28 14:15:21 +0300

    шаблон

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+
+    }
+    public static void task1(){
+        System.out.println("Задача 1.");
+    }
+    public static void task2(){
+        System.out.println("Задача 2.");
+    }
+    public static void task3(){
+        System.out.println("Задача 3.");
+
+    }
+    public static void task4(){
+        System.out.println("Задача 4.");
+    }
+    public static void task5(){
+        System.out.println("Задача 5.");
+    }
+    public static void task6(){
+        System.out.println("Задача 6.");
+    }
+    public static void task7(){
+        System.out.println("Задача 7.");
+    }
+}
