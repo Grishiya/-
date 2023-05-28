f43b8d58ad1f190e2f6cf65785075151d89bd297
Grisha <guruwolf8@gmail.com>
2023-05-28 15:47:28 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+
 public class Main {
+    public static int[][] vulti;
+
     public static void main(String[] args) {
         task1();
         task2();
@@ -9,26 +13,42 @@ public class Main {
         task7();
 
     }
-    public static void task1(){
+
+    public static void task1() {
         System.out.println("Задача 1.");
+        int ageOld = 25;
+        if (ageOld >= 18) {
+            System.out.println("Если возраст человека равен " + ageOld + ", то он совершеннолетний");
+        }
+        int ageChild=17;
+        if (ageChild>=18)
+            System.out.println("Вы совершеннолетний");
+        else
+            System.out.println("Если возраст человека равен "+ageChild+", то он не достиг совршеннолетияБнужно подождать");
     }
-    public static void task2(){
+
+    public static void task2() {
         System.out.println("Задача 2.");
     }
-    public static void task3(){
+
+    public static void task3() {
         System.out.println("Задача 3.");
 
     }
-    public static void task4(){
+
+    public static void task4() {
         System.out.println("Задача 4.");
     }
-    public static void task5(){
+
+    public static void task5() {
         System.out.println("Задача 5.");
     }
-    public static void task6(){
+
+    public static void task6() {
         System.out.println("Задача 6.");
     }
-    public static void task7(){
+
+    public static void task7() {
         System.out.println("Задача 7.");
     }
 }
